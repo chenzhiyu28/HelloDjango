0ffc56e11b7cf620ba8b01e285c87ee81b3b765a
Leopold <czy.leopold97@gmail.com>
2026-01-09 03:32:46 +1000

    fix db connection and add env

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
